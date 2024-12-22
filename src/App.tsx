3e3532cd18ac90a5a169501f3081a64bd5467461
Arif Hosssain <arifma0947@gmail.com>
2024-12-22 14:07:19 +0600

    first

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Hero from "./component/Hero";
+
+function App() {
+  return (
+    <div className="bg-gradient-to-br from-gray-900 to-gray-800 text-white min-h-screen relative overflow-hidden">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
